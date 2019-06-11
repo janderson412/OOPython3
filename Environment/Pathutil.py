5d735e3e789e33f05d947c92468c1713714fbb1e
Anderson, John [CLE] <john.e.anderson@medtronic.com>
2019-06-11 16:44:57 -0400

    Pathutil now checks for duplicates and prints in RED if duplicate found.

--- Environment/Pathutil.py
+++ Environment/Pathutil.py
@@ -1,14 +1,29 @@
 import argparse
 import os
-
+from colorama import Fore, Back, Style, init
 
 class PathComponent:
+    '''Class which encapsulates a component of the PATH environment variable.'''
 
     def __init__(self, name):
+        '''Initialize with component name'''
         self.name = name
+        self.is_duplicate = False
 
+    def check_duplicate(self, other):
+        '''Mark this as duplicate if other has matching name'''
+        if self.name.lower() == other.name.lower():
+            self.is_duplicate = True
+
+    def print(self):
+        '''Print component'''
+        col = Fore.LIGHTCYAN_EX
+        if self.is_duplicate:
+            col = Fore.RED
+        print(col + self.name)
 
 if __name__ == '__main__':
+    init(convert=True)
     parser = argparse.ArgumentParser(description='Display information about the PATH')
     parser.add_argument('--sort', dest='sort', action='store_true', help='Sort the path components alphabetically ('
                                                                          'case insensitive)')
@@ -17,13 +32,21 @@ if __name__ == '__main__':
 
     env = os.environ
     path = env['PATH']
+
     if args.sort:
         components = sorted(path.split(sep=';'), key=lambda s: s.lower())
     else:
         components = path.split(sep=';')
 
-    for x in components:
-        print(x)
+    component_list = [PathComponent(i) for i in components if len(i) > 0]
+
+    if args.showdups:
+        for n in range(0, len(component_list) - 1):
+            for x in range(n + 1, len(component_list)):
+                component_list[x].check_duplicate(component_list[n])
+
+    for x in component_list:
+        x.print()
 
 
 
