80fe3b09e793172add868db75bf4cfbadd4db62d
Anderson, John [CLE] <john.e.anderson@medtronic.com>
2019-06-28 11:13:18 -0400

    Add pandas code.

---
+++ Pandas/pandas_csv.py
@@ -0,0 +1,13 @@
+
+from datetime import datetime
+from pandas import DataFrame, Series
+from pandas import DatetimeIndex
+
+s = Series([1,2,3,4], index=['a','b','c','d'])
+print(s)
+
+import random
+
+data = [random.randint(0, 10000) for x in range(10000)]
+
+
